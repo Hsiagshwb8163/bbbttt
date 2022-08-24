9bd7bd123d5213d502df55a6071e9ab6a066232f
Hsiagshwb8163 <111885216+Hsiagshwb8163@users.noreply.github.com>
2022-08-24 19:34:07 +0600

    Create aaaa.py

---
+++ aaaa.py
@@ -0,0 +1,21 @@
+from telegram.ext import *
+import time
+
+def hl(a,b):
+    m =  a.message.reply_text("Hello")
+    time.sleep(1)
+    mm = m.edit_text("Please Wait")
+    time.sleep(1)
+    mm = m.edit_text(mm.text+"\nPleasee......")
+    time.sleep(1)
+    m.edit_text(mm.text+"\nOh Nothing")
+    
+    print(a.message.from_user)
+
+
+updater = Updater("5691804740:AAFc1Vvf16R_iYAFJfEFvA_L7HL-p5n1qjk", use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler("start",hl))
+
+updater.start_polling()
+updater.idle()
